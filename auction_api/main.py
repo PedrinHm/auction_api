2d189fcbecf75e6d59ed483171e920aa6234f11f
PedrinHm <pedrohm441@gmail.com>
2024-06-08 21:28:13 -0300

    teste

--- auction_api/main.py
+++ auction_api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from auction_api.routers import items, bids, users
 from .database import Base, engine
 from . import models
+import logging
 
 app = FastAPI()
 
@@ -13,11 +14,17 @@ app.include_router(users.router)
 def read_root():
     return {"message": "Welcome to API"}
 
+logging.basicConfig(level=logging.INFO)
+
 def create_tables():
+    logging.info("Creating tables...")
     Base.metadata.create_all(bind=engine)
+    logging.info("Tables created successfully.")
 
 def init_db():
+    logging.info("Initializing database...")
     models.Base.metadata.create_all(bind=engine)
+    logging.info("Database initialized successfully.")
 
 if __name__ == "__main__":
     create_tables()
